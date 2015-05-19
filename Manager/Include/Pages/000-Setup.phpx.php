56c93699642bd934369f5b693f2b67e32130cb89
alcexhim <alcexhim@gmail.com>
2015-05-19 15:52:51 -0400

    Begin to implement Setup page

--- Manager/Include/Pages/000-Setup.phpx.php
+++ Manager/Include/Pages/000-Setup.phpx.php
@@ -1,8 +1,27 @@
 <?php
 	namespace StudioAnywhere\Tenant\Pages;
 	
-	class SetupPage
+	use Phast\Parser;
+	use Phast\System;
+	
+	class SetupPage extends PhastPage
 	{
+		public function OnInitializing()
+		{
+			
+		}
 		
+		private function WriteConfigurationFile($values)
+		{
+			$filename = System::GetRootPath() . "/Include/Configuration.inc.php";
+			$file = fopen($filename, "w");
+			fwrite($file, "<?php\n");
+			fwrite($file, "\tuse Phast\\System;\n\n");
+			foreach ($values as $key => $value)
+			{
+				fwrite($file, "\tSystem::SetConfigurationValue(\"" . $key . "\", \"" . $value . "\");\n");
+			}
+			fflush($file);
+		}
 	}
 ?>
