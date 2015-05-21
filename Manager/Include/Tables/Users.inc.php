069bdfd6a71a240ab5407a3cabab69aa381413e1
alcexhim <alcexhim@gmail.com>
2015-05-21 14:07:45 -0400

    Define the Users table

---
+++ Manager/Include/Tables/Users.inc.php
@@ -0,0 +1,13 @@
+<?php
+	use Phast\Data\Table;
+	use Phast\Data\Column;
+	
+	$tables[] = new Table("Users", "user_", array
+	(
+		new Column("ID", "INT", null, false, true),
+		new Column("UserName", "VARCHAR", 50),
+		new Column("DisplayName", "VARCHAR", 50),
+		new Column("PasswordHash", "VARCHAR", 256),
+		new Column("PasswordSalt", "VARCHAR", 16)
+	));
+?>
