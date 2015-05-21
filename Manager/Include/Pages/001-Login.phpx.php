1d045e1839377a98d030a79018e39ce5e7c8a8f1
alcexhim <alcexhim@gmail.com>
2015-05-21 15:57:09 -0400

    Add legal notice text to bottom of Login page

--- Manager/Include/Pages/001-Login.phpx.php
+++ Manager/Include/Pages/001-Login.phpx.php
@@ -1,10 +1,17 @@
 <?php
 	namespace StudioAnywhere\Tenant\Pages;
+
+	use Phast\CancelEventArgs;
+	use Phast\System;
 	
 	use Phast\Parser\PhastPage;
-	
+		
 	class LoginPage extends PhastPage
 	{
+		public function OnInitializing(CancelEventArgs $e)
+		{
+			$e->RenderingPage->GetControlByID("paraCopyright")->InnerHTML = System::GetConfigurationValue("Application.CopyrightMessage");
+		}
 		
 	}
 ?>
